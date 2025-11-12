7be2f78cbfbe340c5c39f1b2dc44d8053b25b40d
ulrich <info@fastfreightlines.online>
2025-11-12 12:23:08 +0100

    delete this file

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,19 +1,1 @@
-import jwt from "jsonwebtoken";
 
-const JWT_SECRET = "supersecretkey";
-
-export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
-
-    try{
-        
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        return res.status(403).json({ message: "Invalid token" })
-    }
-};
