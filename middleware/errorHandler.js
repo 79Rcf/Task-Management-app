2043aedc5ce6bbbda1863a29a6a77aaf94be4c78
ulrich <info@fastfreightlines.online>
2025-11-12 12:26:16 +0100

    Delete errorHandler middleware
    Remove errorHandler middleware function.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,15 +1,1 @@
-export const errorHandler = (err, req, res, next) => {
-    console.error("ERROR:", err.stack);
-  
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-  
-    res.status(statusCode).json({
-      success: false,
-      status: statusCode,
-      message,
 
-      stack: process.env.NODE_ENV === "development" ? err.stack : undefined
-    });
-  };
-  
