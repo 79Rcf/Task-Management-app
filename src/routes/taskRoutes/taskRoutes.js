b4747d5e403d818d4a75c13b2d79d1186ab56f9f
Ulrich Killian <fastfrightshipping@gmail.com>
2025-11-12 12:49:33 +0100

    taskRoutes update using id to with put endpoint to get task created with id

--- src/routes/taskRoutes/taskRoutes.js
+++ src/routes/taskRoutes/taskRoutes.js
@@ -15,23 +15,3 @@ router.put('/:id', protect, updateTaskStatus);
 router.delete('/:id', protect, deleteTask);
 
 export default router;
-
-/* testing purpose */
-/* 
-import express from 'express';
-import { 
-    getTasks, 
-    createTask, 
-    updateTaskStatus, 
-    deleteTask 
-} from '../../controllers/taskController.js';
-
-const router = express.Router();
-
-router.get('/', getTasks);
-router.post('/', createTask);
-router.put('/:id', updateTaskStatus);
-router.delete('/:id', deleteTask);
-
-export default router;
- */
